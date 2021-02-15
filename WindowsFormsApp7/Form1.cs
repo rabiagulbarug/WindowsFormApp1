04cde19bfde07ebcf0379f5c8c7e654ee12046fa
Rabia Gül Baruğ <rabiabrg7@gmail.com>
2021-02-15 23:35:36 +0300

    your commit message

---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //WindowState = FormWindowState.Minimized;
+            if (WindowState == FormWindowState.Maximized)
+                WindowState = FormWindowState.Normal;
+            else
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double tutar = Convert.ToDouble(textBox1.Text);
+            double KDV=0,toplam;
+            if(radioButton1.Checked)
+            {
+                KDV = tutar * 0.08;
+            }
+            else if (radioButton2.Checked)
+            {
+                KDV = tutar * 0.18;
+            }
+            toplam = tutar + KDV;
+            textBox2.Text = toplam.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp7/login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="a"&&textBox2.Text=="1")
+            {
+                //MessageBox.Show("Giriş Başarılı");
+                Form1 frm = new Form1();
+                frm.Show();
+                Hide();
+            }
+            else
+            { 
+                MessageBox.Show("Giriş Başarısız");
+                textBox1.Text = String.Empty;
+                textBox2.Text = String.Empty;
+            }
+
+        }
+    }
+}
